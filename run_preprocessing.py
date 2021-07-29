365e70cf5c5aaf8ac867464f1fe59dc553d2aed8
Michael Wellner <michaelwellner@kpmg.com>
2021-07-29 21:55:31 +0200

    sc

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -2,7 +2,7 @@ from gsum.config import GuidedSummarizationConfig
 from gsum.summarizer import GuidedSummarizationDataModule
 
 if __name__ == "__main__":
-    config = GuidedSummarizationConfig.apply('mlsum', 'bert', False, guidance_signal='oracle')
-    dat = GuidedSummarizationDataModule(config)
+    config = GuidedSummarizationConfig.apply('mlsum', 'multi', True, guidance_signal='oracle')
+    dat = GuidedSummarizationDataModule(config, is_extractive=False)
 
     dat.prepare_data()
